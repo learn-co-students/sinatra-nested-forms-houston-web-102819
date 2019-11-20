ec0bf341ed71223cbbab361aaa308005d608e58f
Nbuck3905 <Nathanbuchanan671@gmail.com>
2019-11-19 18:00:50 -0600

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+    attr_accessor :name, :type, :booty
+    @@all = []
+
+    def initialize(name,type,booty)
+        @name = name
+        @type = type
+        @booty = booty
+        @@all.push(self)
 end
+
+    def self.all
+        return @@all
+    end 
+
+    def self.clear
+        @@all = []
+    end
+end    
